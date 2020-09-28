e20f9d734cd921531a19370dbe49dfc9954b1485
glennrv <glennrv@lyse.net>
2020-09-28 21:27:42 +0200

    - Removed unnecessary 2 functions - Added comments to code

--- Adventure_Text.py
+++ Adventure_Text.py
@@ -4,21 +4,8 @@ import math
 
 
 # Functions
-def date_of_birth(string):
-    dob_int = []
-    if len(string) == 10 and string[2] == '-' and string[5] == '-':
-        dob = string.split('-')
-        for i in dob:
-            dob_int.append(int(i))
-    return dob_int
-
-
-def first_last_name(string):
-    names = string.split()
-    return names
-
-
 def new_user(dictionary):
+    """Adds a new user with all the default settings."""
     new_username = input("Please enter the new users username: ")
     new_user_password = input("Please enter the new users password: ")
     with open("valid_users.pickle", "wb") as vuw:
@@ -32,7 +19,19 @@ def new_user(dictionary):
     return print("New user added.")
 
 
+def add_to_inv(item, amount):
+    """Adds item and amount to user's inventory"""
+    with open("valid_users.pickle", "wb") as vuw:
+        if item in user['inventory']:
+            user['inventory'][item] += amount
+            pickle.dump(valid_users, vuw)  # VIKTIG!!!!!!!
+        else:
+            user['inventory'][item] = amount
+            pickle.dump(valid_users, vuw)  # VIKTIG!!!!!!!
+
+
 def fish_options(user):
+    """Views the avaliable fishing options based on user's fishing level"""
     lvl = user['fishing level']
     if lvl < 5:
         return fish_methods[:1]
@@ -43,6 +42,7 @@ def fish_options(user):
 
 
 def fish(user, method):
+    """Function for the fishing skill, executed based on chosen method"""
     lvl = user['fishing level']
     while True:
         if method == 'small net':
@@ -84,22 +84,18 @@ def fish(user, method):
             return print('The option you chose is not avaliable to you.')
 
 
-def add_to_inv(item, amount):
-    with open("valid_users.pickle", "wb") as vuw:
-        if item in user['inventory']:
-            user['inventory'][item] += amount
-            pickle.dump(valid_users, vuw)  # VIKTIG!!!!!!!
-        else:
-            user['inventory'][item] = amount
-            pickle.dump(valid_users, vuw)  # VIKTIG!!!!!!!
+### Program setup
 
-
-# Program setup
+# The currently avaliable fishing methods
 fish_methods = ['small net', 'spear', 'fishing rod']
+
+# Loads in the file containing all the valid users and their stats
 valid_users_in = open("valid_users.pickle", "rb")
 valid_users = pickle.load(valid_users_in)
 
-# Game loop
+### Game loop
+
+# Main Menu loop - Login screen
 while True:
     print("""
     Welcome to Adventure Text
@@ -113,12 +109,18 @@ while True:
         valid_users_in.close()
         break
 
+    # Checks if username and password match, and logs user in.
     if username in valid_users and password == valid_users[username]['password']:
         user = valid_users[username]
         print(f"""Welcome, {username}!""")
+
+        # Logged in game loop
         while True:
+
+            # Command input which is the base of every action in-game
             command = input("Type Command: ").lower()
 
+            # Dictionary of basic print commands
             commands = {
                 'help': """
                 List of avaliable commands:
@@ -127,45 +129,62 @@ while True:
                     Go fish - Initiates fishing mini-game.
                     Logout - Log out to main screen.""",
                 'test_inv': f"""
-                {user['inventory']}""",   # TEST KOMMANDO - SKAL ENDRES!
+                {user['inventory']}""",   # TEST COMMAND - WILL BE CHANGED!
                 'fish_lvl': f"""Your fishing level is: {user['fishing level']}""",
                 'fish_exp': f"""Your fishing experience is: {user['fishing exp']}/{user['fishing level'] * 50}"""
             }
 
+            # Logout command
             if command == 'logout':
                 print("""
             Thanks for now, hope to see you again later!""")
                 break
 
+            # Checks if the command is a basic print command
             elif command in commands:
                 print(commands[command])
 
+            # Adds a new user
             elif command == "new user":
                 new_user(valid_users)
 
-            elif command == 'add_to_inv':   # TEST KOMMANDO - SKAL SLETTES!
+            # Admin command that adds item to inventory (used for testing)
+            elif command == 'add_to_inv':   # TEST COMMAND - WILL BE DELETED!
                 item = input('Item: ')
                 amount = int(input('Amount: '))
                 add_to_inv(item, amount)
                 print('Item successfully added.')
 
+            # Command to initiate fishing skill/mini-game
             elif command == 'go fish':
                 print("""\nYou go to the nearest lake to fish.""")
+
+                # Fishing game-loop
                 while True:
                     method = input("""
     What would you like to do at the lake?
     """).lower()
+
+                    # Exits the fishing game-loop, back to logged-in game-loop
                     if method == 'leave':
                         break
+
+                    # Prints avaliable commands within the fishing game-loop
                     elif method == 'help':
                         print("""
         Help - shows avaliable commands.
         Options - shows avaliable fishing options.
         *option* - initiates fishing with said option.""")
+
+                    # Prints avaliable fishing methods for user's level
                     elif method == 'options':
                         print(fish_options(user))
+
+                    # Initiates fishing
                     elif method in fish_methods:
                         fish(user, method)
+
+                        # Checks if user's fishing exp is high enough to level up
                         if user['fishing exp'] >= (user['fishing level'] * 50):
                             user['fishing level'] += 1
                             user['fishing exp'] -= (user['fishing level'] - 1) * 50
